6f7a0270b8c435dd9f8fc1520f05822f8e335d9b
Alexander Smith <mr.alexander.smith@hotmail.com>
2024-02-20 18:01:32 +0300

    Implemented database connection setup in Util.java using properties from database.properties file

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,27 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Properties properties = new Properties();
+
+        try (InputStream inputStream = Files.newInputStream(
+                Paths.get("database.properties"))) {
+            properties.load(inputStream);
+        } catch (IOException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+        return DriverManager.getConnection(properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password"));
+    }
 }
