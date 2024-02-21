1174537220c5cc4ebcc37cbaadbd59318aa28278
Alexander Smith <mr.alexander.smith@hotmail.com>
2024-02-21 23:55:41 +0300

    Added Property.java for loading and retrieving database properties, and reformatted database.properties for compatibility

---
+++ src/main/java/jm/task/core/jdbc/util/Property.java
@@ -0,0 +1,31 @@
+package jm.task.core.jdbc.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public final class Property {
+    private static final Properties PROPERTIES = new Properties();
+
+    static {
+        loadProperties();
+    }
+
+    private Property() {
+    }
+
+    public static String get(String stringKey) {
+        return PROPERTIES.getProperty(stringKey);
+    }
+
+    private static void loadProperties() {
+        try (InputStream inputStream = Files.newInputStream(
+                Paths.get("database.properties"))) {
+            PROPERTIES.load(inputStream);
+        } catch (IOException exception) {
+            throw new RuntimeException(exception);
+        }
+    }
+}
